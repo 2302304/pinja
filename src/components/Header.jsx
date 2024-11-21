c34a6c9b6e11a89838537bea9230449a6d6907b0
Jani Harju <e2302304@edu.vamk.fi>
2024-11-21 12:14:31 +0200

    Headerin sisällön luominen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUsers, faChartLine } from '@fortawesome/free-solid-svg-icons';
+import PinjaLogo from '../assets/Pinja-logo.webp'; // Importoi Pinjan logo
 
 const Header = () => {
   return (
     <header className="header">
-      <h1>Pinjan Konsulttien Hallinta</h1>
+      <div className="logo">
+        <img src={PinjaLogo} alt="Pinja Logo" className="pinja-logo" /> {/* Lisää Pinjan logo */}
+      </div>
+      <nav className="nav-links">
+        <Link to="/" className="nav-link">
+          <FontAwesomeIcon icon={faHome} /> Etusivu
+        </Link>
+        <Link to="/consultants" className="nav-link">
+          <FontAwesomeIcon icon={faUsers} /> Konsultit
+        </Link>
+        <Link to="/dashboard" className="nav-link">
+          <FontAwesomeIcon icon={faChartLine} /> Hallintapaneeli
+        </Link>
+      </nav>
     </header>
   );
 };
